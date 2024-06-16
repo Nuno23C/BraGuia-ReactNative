3195c1fa2bc412a87909333328e8a5c9dae50a3f
Goncalocp <goncalocp02@gmail.com>
2024-06-16 21:38:25 +0100

    .

--- android/app/src/main/java/com/braguiarn/MainApplication.kt
+++ android/app/src/main/java/com/braguiarn/MainApplication.kt
@@ -1,46 +1,40 @@
 package com.braguiarn
 
 import android.app.Application
-import android.content.Intent
-import com.braguiarn.location.LocationModule // Importe o LocationModule aqui
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.shell.MainReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-    override val reactNativeHost: ReactNativeHost =
-        object : ReactNativeHost(this) {
-            override fun getPackages(): List<ReactPackage> {
-                val packages = PackageList(this).packages.toMutableList()
-                packages.add(LocationModule(reactContext)) // Adicione o LocationModule aqui
-                return packages
+  override val reactNativeHost: ReactNativeHost =
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).packages.apply {
             }
 
-            override fun getJSMainModuleName(): String = "index"
+        override fun getJSMainModuleName(): String = "index"
 
-            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-        }
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-    override fun onCreate() {
-        super.onCreate()
-        SoLoader.init(this, false)
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+      }
 
-        // Inicie o serviço de localização em segundo plano
-        startLocationService()
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this, false)
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      load()
     }
-
-    private fun startLocationService() {
-        val serviceIntent = Intent(this, LocationService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Inicie o serviço de maneira que ele continue em segundo plano mesmo que o aplicativo seja fechado
-            startForegroundService(serviceIntent)
-        } else {
-            startService(serviceIntent)
-        }
-    }
+  }
 }
